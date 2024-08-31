63ceec3f7aa389ffb524f0b9f468e198d1947d7b
JeremyArt3mis <jeremyartemis5@gmail.com>
2024-08-31 13:56:07 -0700

    Update player.py

--- player.py
+++ player.py
@@ -29,9 +29,8 @@ class Player:
             self.max_frame = 10
         player_frame = pygame.transform.flip(pygame.transform.scale(self.sprite_sheet.get_frame(self.action,math.floor(self.frame % self.max_frame)),(288 * 2.5,128 * 2.5)),face_left,False)
         self.game.screen.blit(player_frame,(self.x,self.y))
-        self.player_rect = pygame.Rect(self.x + ,self.y,128,288)
-        print(self.x + 288,self.y + 128)
-        pygame.draw.rect(self.game.screen,(10,50,200),self.player_rect)
+        self.player_rect = pygame.Rect(self.x + 345,self.y + 200,30,120)
+        #pygame.draw.rect(self.game.screen,(10,50,200),self.player_rect)
         self.frame += 0.2
         
     
@@ -39,11 +38,23 @@ class Player:
         keys = pygame.key.get_pressed()
         self.xspeed += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * 0.75
         
+        
 
     def update(self):
+        keys = pygame.key.get_pressed()
+        self.yspeed += 0.5
+        if self.player_rect.colliderect(self.game.test_rect):
+            while self.player_rect.colliderect(self.game.test_rect):
+                self.player_rect.y -= 1
+            self.y = self.player_rect.y - 199
+            self.yspeed = 0
+            self.yspeed += -15 * keys[pygame.K_UP]
+        self.y += self.yspeed
+        print(self.y,self.x)
         self.x += self.xspeed
         self.xspeed *= 0.9
-        self.y += self.yspeed
-        if not self.player_rect.colliderect(self.game.test_rect):
-           self.yspeed -= self.yspeed
+        
+        
+        
+            
         
