73a476c9db21d0df4ee28b50424730951dda7861
JeremyArt3mis <jeremyartemis5@gmail.com>
2024-09-14 13:39:18 -0700

    Update player.py

--- player.py
+++ player.py
@@ -19,14 +19,20 @@ class Player:
     
     def render(self):
         face_left = self.xspeed < 0
-        if abs(self.xspeed) < 1.5:
+        if self.yspeed > 0:
+            self.action = "fall"
+        elif self.yspeed < 0:
+            self.action = "jump"
+        elif abs(self.xspeed) < 1.5:
             self.action = "idle"
         else:
             self.action = "run" 
         if self.action == "idle":
             self.max_frame = 12
-        else:
+        elif self.action == "run":
             self.max_frame = 10
+        else:
+            self.max_frame = 2
         player_frame = pygame.transform.flip(pygame.transform.scale(self.sprite_sheet.get_frame(self.action,math.floor(self.frame % self.max_frame)),(288 * 2.5,128 * 2.5)),face_left,False)
         self.game.screen.blit(player_frame,(self.x,self.y))
         self.player_rect = pygame.Rect(self.x + 345,self.y + 200,30,120)
@@ -45,14 +51,18 @@ class Player:
         self.yspeed += 0.5
         if self.player_rect.colliderect(self.game.test_rect):
             while self.player_rect.colliderect(self.game.test_rect):
-                self.player_rect.y -= 1
+                self.player_rect.y -= self.yspeed / abs(self.yspeed)
             self.y = self.player_rect.y - 199
             self.yspeed = 0
             self.yspeed += -15 * keys[pygame.K_UP]
         self.y += self.yspeed
-        print(self.y,self.x)
         self.x += self.xspeed
         self.xspeed *= 0.9
+        
+        
+        
+            
+        
         
         
         
