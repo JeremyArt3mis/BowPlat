f97c0778e1d188639dbe486501f961ea02412b94
JeremyArt3mis <jeremyartemis5@gmail.com>
2024-10-05 13:59:50 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -3,6 +3,7 @@ import pygame,sys
 from sprites import sprites
 from player import Player
 from projectile import Projectile
+from goblin import Goblin
 
 class Game:
     def __init__(self):
@@ -14,9 +15,10 @@ class Game:
         
         #BUGGIES
         self.sprite_sheet = sprites(pygame.image.load("Player_Assets/animations/spritesheets/Player_Sheet_288x128.png"),100)
-        
+        self.goblin_sheet = sprites(pygame.image.load("Enemy_Assets/Goblin/Attack3.png"),12)
         self.test_rect = pygame.Rect(0,900,1700,200)
         self.player = Player(500,300,self.sprite_sheet,self)
+        self.goblin = Goblin(500,300,self.goblin_sheet.img,self)
     def run(self):
         while True:
             self.clock.tick(60)
@@ -37,6 +39,7 @@ class Game:
     def render(self):
         self.screen.fill((167,182,170))
         self.player.render()
+        self.goblin.render()
         for arrow in self.player.active_projectiles:
             arrow.update(self.player.dir)
             arrow.render()
